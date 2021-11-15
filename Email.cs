99365fc66973c13542501f06478bd64f62e20118
crazy-medic <vineethtv001@outlook.com>
2021-11-15 10:00:16 +0530

    Upload of email login

---
+++ Email.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace UserrRegistrationProgram
+{
+    class Email
+    {
+        const string Regex_email = "^[a-z]{3,}[.]*[a-z0-9]*[@]{1}[a-z]{2,}[.]{1}[co]{2}[m]*[.]*[a-z]*$";
+        public string GetEMailID()
+        {
+            Console.WriteLine("Enter the first name:");
+            string eMailID = Console.ReadLine();
+            if (string.IsNullOrEmpty(eMailID))
+            {
+                Console.WriteLine("Please enter an email!");
+                GetEMailID();
+            }
+            bool output = TestEMail(eMailID);
+            if (output == true)
+            {
+                return eMailID;
+            }
+            else
+            {
+                Console.WriteLine("Invalid email entered!");
+                GetEMailID();
+            }
+            return eMailID;
+        }
+        public static bool TestEMail(string eMailID) => (Regex.IsMatch(eMailID, Regex_email));
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,12 +7,15 @@ namespace UserrRegistrationProgram
         
         static void Main(string[] args)
         {
-            string FName, LName;
-            FirstName fn = new FirstName();
-            FName = fn.GetFirstName();
-            LastName ln = new LastName();
-            LName = ln.GetLastName();
-            Console.WriteLine("\nYour name is " + FName + " "+LName);
+            string FName, LName,eMailid;
+            //FirstName fn = new FirstName();
+            //FName = fn.GetFirstName();
+            //LastName ln = new LastName();
+            //LName = ln.GetLastName();
+            //Console.WriteLine("\nYour name is " + FName + " "+LName);
+            Email em = new Email();
+            eMailid = em.GetEMailID();
+            Console.WriteLine("Entered email is "+eMailid);
         }
     }
 }
